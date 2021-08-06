f8f1006ca1d544f7d095606258686c0de0583658
gustavorooxx <gust_chagas@outlook.com>
2021-08-06 16:38:43 -0300

    renderizando pagina checkout

--- controllers/pagamentoController.js
+++ controllers/pagamentoController.js
@@ -1,14 +1,10 @@
 const PagamentoController = {
     payment: (req, res) => {
-<<<<<<< HEAD
-        res.render('paginaPagamento')
+        res.render('pagamento')
     },
 
     checkout: (req, res) => {
         res.render('checkout')
-=======
-        res.render('pagamento')
->>>>>>> 8bc1a8bcfb00e0a5d05f1645368b882874b8e504
     }
 }
 
