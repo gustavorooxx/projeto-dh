43b8dde9e3b64130dda5a67f46fc40aebbf4fedb
P-Schil <schil.ambiental@gmail.com>
2021-10-14 11:55:48 -0300

    inserindo pagamento seeders

---
+++ database/seeders/20211014100555-pagamento.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return await queryInterface.bulkInsert('pedido', [
+      {
+         id: '1',
+         tokenPagamento: '81dc9bdb52d04dc20036dbd8313ed055',
+         pedido_id:'1',
+         usuario_id: '2',
+         
+      },
+      {
+        id: '2',
+        tokenPagamento: 'b9a4a9706b4c1c6dd4ac5dcb54b96204',
+        pedido_id:'2',
+        usuario_id: '1',
+
+     },
+     {
+      id: '3',
+      tokenPagamento: '78b91366b15c399bd05530e96d28a530',
+      pedido_id:'3',
+      usuario_id: '3',
+   },
+    
+    ]);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    
+    return await queryInterface.bulkDelete('pagamento', null);
+  }
+};
+
